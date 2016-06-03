c470bf6ef761ff79e7241b1528b2886ea8775210
USER <user@D-10-156-23-31.dhcp4.washington.edu>
2016-06-03 12:11:34 -0700

    New folder for Program 4

---
+++ P4/P4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  P4
+//
+//  Created by USER on 6/3/16.
+//  Copyright (c) 2016 Shane. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
