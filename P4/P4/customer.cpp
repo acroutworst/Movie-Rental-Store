745882279d459091c9b6e56588d3576396f3e86c
USER <user@D-10-156-23-31.dhcp4.washington.edu>
2016-06-03 12:34:29 -0700

    added customer class

---
+++ P4/P4/customer.cpp
@@ -0,0 +1,39 @@
+//
+//  customer.cpp
+//  P4
+//
+//  Created by USER on 6/3/16.
+//  Copyright (c) 2016 Shane. All rights reserved.
+//
+
+#include "customer.h"
+
+Customer::Customer()
+{
+    
+}
+
+Customer::~Customer()
+{
+    
+}
+
+void Customer::buildCustomerList(ifstream&)
+{
+    
+}
+
+void Customer::displayHistory(int custID)
+{
+  
+}
+
+void Customer::addHistory(string movieName) //add movie to customer’s borrowing history
+{
+    
+}
+
+bool Customer::isCustomer(int custID) //check if valid customer
+{
+    return true;
+}
---
+++ P4/P4/customer.h
@@ -0,0 +1,36 @@
+//
+//  customer.h
+//  P4
+//
+//  Created by USER on 6/3/16.
+//  Copyright (c) 2016 Shane. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Customer
+{
+public:
+    Customer (); //default constructor
+    ~Customer (); //deconstructor
+    
+    void buildCustomerList(ifstream&); //read information from file, build customerList
+    void displayHistory(int custID); //display customer borrowing history
+    void addHistory(string movieName); //add movie to customer’s borrowing history
+    bool isCustomer(int custID); //check if valid customer
+    
+private:
+    struct cust
+    {
+        int custID; //customer ID
+        string lastName; //customer last name
+        string firstName; //customer first name
+        vector<string> history; //customers borrowing history
+    };
+    
+    vector<cust> customerList;	//list of customers
+};
