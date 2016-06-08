796af1e9a4831fe400d633b0ba3a552695e701ac
Adam <croutworsta@gmail.com>
2016-06-07 20:03:00 -0700

    added comments to classic

--- P4/P4/classic.cpp
+++ P4/P4/classic.cpp
@@ -45,24 +45,25 @@ ClassicMovie::~ClassicMovie()
 {
 }
 
-// --------------------- Set Dat -------------------------------------------
-// Default constructor
-// Preconditions: none
-// Postconditions: none
+// --------------------- Set Data -------------------------------------------
+// setData: Set inventory data
+// Preconditions: An input stream from movie file
+// Postconditions: A string of actor first/last names
+//				   and release month and year
 // -----------------------------------------------------------------------------
 void ClassicMovie::setData(istream& infile) 
 {
 	infile.get();
-	getline(infile, director, ',');
+	getline(infile, director, ',');	// Read director
 
-	if (director.size() > 18) {
+	if (director.size() > 18) {		// Resize width if text is too large
 		title.resize(18);
 	}
 
 	infile.get();
-	getline(infile, title, ',');
+	getline(infile, title, ',');	// Read title	
 
-	if (title.size() > 22) {
+	if (title.size() > 22) {		// Resize width if text is too large
 		title.resize(22);
 	}
 
@@ -71,10 +72,10 @@ void ClassicMovie::setData(istream& infile)
 	infile >> month >> year;
 }
 
-// --------------------- Inventory() -------------------------------------------
-// Default constructor
-// Preconditions: none
-// Postconditions: none
+// --------------------- Set Data -------------------------------------------
+// setDataTwice: Set additional inventory data
+// Preconditions: Input file stream
+// Postconditions: string wit month, year, first, and last name
 // -----------------------------------------------------------------------------
 void ClassicMovie::setDataTwice(istream& infile) 
 {
@@ -85,10 +86,10 @@ void ClassicMovie::setDataTwice(istream& infile)
 
 }
 
-// --------------------- Inventory() -------------------------------------------
-// Default constructor
+// --------------------- Display -------------------------------------------
+// Display: display classic movie data 
 // Preconditions: none
-// Postconditions: none
+// Postconditions: String of classic data from title to actor last name
 // -----------------------------------------------------------------------------
 void ClassicMovie::display() const 
 {
@@ -99,10 +100,10 @@ void ClassicMovie::display() const
 		<< actorFirst << " " << actorLast << endl;
 }
 
-// --------------------- Inventory() -------------------------------------------
-// Default constructor
+// --------------------- Display Header -------------------------------------------
+// displayHeader: Display title / header for each classic
 // Preconditions: none
-// Postconditions: none
+// Postconditions: An output displaying at the top of the classic movie section
 // -----------------------------------------------------------------------------
 void ClassicMovie::displayHeader() const
 {
@@ -113,30 +114,32 @@ void ClassicMovie::displayHeader() const
 		<< endl;
 }
 
-// --------------------- Inventory() -------------------------------------------
-// Default constructor
-// Preconditions: none
-// Postconditions: none
+// --------------------- Operator== -------------------------------------------
+// operator==: check to see if current classic movie is equivalent to the one 
+//			   in the inventory
+// Preconditions: Inventory object
+// Postconditions: Boolean for equivalence check of classic movie
 // -----------------------------------------------------------------------------
 bool ClassicMovie::operator==(const Inventory& inv) const 
 {
 
-	const ClassicMovie& aClassic = static_cast<const ClassicMovie&>(inv);
+	const ClassicMovie& aClassic = static_cast<const ClassicMovie&>(inv); // Cast inventory as classic movie
 
 	return (year == aClassic.year && month == aClassic.month
 		&& actorFirst == aClassic.actorFirst &&
 		actorLast == aClassic.actorLast);
 }
 
-// --------------------- Inventory() -------------------------------------------
-// Default constructor
-// Preconditions: none
-// Postconditions: none
+// --------------------- Operator< -------------------------------------------
+// Operator<: This method is used to organize the movie by year and actor
+// Preconditions: Inventory object 
+// Postconditions: Boolean saying if current classic is older or newer than 
+//					the one passed through
 // -----------------------------------------------------------------------------
 bool ClassicMovie::operator<(const Inventory& inv) const
 {
-	const ClassicMovie& aClassic = static_cast<const ClassicMovie&>
-		(inv);
+	const ClassicMovie& aClassic = static_cast<const ClassicMovie&>(inv); // Cast inventory as classic movie
+
 
 	if (year < aClassic.year)
 		return true;
@@ -157,12 +160,12 @@ bool ClassicMovie::operator<(const Inventory& inv) const
 		return false;
 }
 
-// --------------------- Inventory() -------------------------------------------
-// Default constructor
+// --------------------- Create -------------------------------------------
+// create: Return instance of classic movie
 // Preconditions: none
-// Postconditions: none
+// Postconditions: Instance of ClassicMovie
 // -----------------------------------------------------------------------------
 Inventory * ClassicMovie::create() 
 {
-	return new ClassicMovie();
+	return new ClassicMovie(); // Return new instance of classic movie
 }
